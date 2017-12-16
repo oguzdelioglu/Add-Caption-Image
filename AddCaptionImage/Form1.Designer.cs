2a6aaee692534c3c63fe13ac41ac4efd0792df29
oguzdelioglu <kemerkaya@hotmail.com.tr>
2017-12-16 09:55:19 +0300

    Add Caption Image
    Add Caption Image

---
+++ AddCaptionImage/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AddCaptionImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.link = new System.Windows.Forms.TextBox();
+            this.yazı = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wait = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.wb);
+            this.groupBox1.Location = new System.Drawing.Point(6, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 432);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Web";
+            // 
+            // wb
+            // 
+            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb.Location = new System.Drawing.Point(3, 16);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.ScriptErrorsSuppressed = true;
+            this.wb.Size = new System.Drawing.Size(426, 413);
+            this.wb.TabIndex = 0;
+            // 
+            // link
+            // 
+            this.link.Location = new System.Drawing.Point(132, 6);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(306, 20);
+            this.link.TabIndex = 1;
+            // 
+            // yazı
+            // 
+            this.yazı.Location = new System.Drawing.Point(132, 32);
+            this.yazı.Name = "yazı";
+            this.yazı.Size = new System.Drawing.Size(306, 20);
+            this.yazı.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your Original Image Link:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Caption:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(432, 63);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wait
+            // 
+            this.wait.Tick += new System.EventHandler(this.wait_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 563);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yazı);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Caption Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.TextBox link;
+        private System.Windows.Forms.TextBox yazı;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer wait;
+    }
+}
+
---
+++ AddCaptionImage/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddCaptionImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void beklet(int kacsn)
+        {
+            wait.Interval = kacsn * 1000;
+            wait.Enabled = true;
+            while (wait.Enabled == true)
+            {
+                System.Windows.Forms.Application.DoEvents();
+            }
+        }
+        private static string CleanFileName(string fileName)
+        {
+            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, c) => current.Replace(c.ToString(), string.Empty));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            seturl();
+        }
+        public void wbbekle()
+        {
+            while (wb.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+        }
+        public void seturl()
+        {
+            link.Text = link.Text.Replace("https", "http");
+          editor:
+            wb.Navigate("http://wigflip.com/automotivator/?src=editor");
+            try
+            {
+                beklet(6);
+                wb.Document.GetElementById("transloadUrl").InnerText = link.Text;
+                wb.Document.Forms[2].InvokeMember("submit");
+                beklet(5);
+            }
+            catch
+            {
+                goto editor;
+            }
+
+            try
+            {
+                HtmlElement textArea = wb.Document.All["text"];
+                if (textArea != null)
+                {
+                    textArea.InnerText = yazı.Text;
+                }
+                HtmlElementCollection col = wb.Document.GetElementsByTagName("input");
+                foreach (HtmlElement helemnt in col)
+                {
+                    if (helemnt.GetAttribute("value") == "Preview")
+                    {
+                        helemnt.InvokeMember("Click");
+                    }
+
+                    if (helemnt.GetAttribute("id") == "saveButton")
+                    {
+                        beklet(3);
+                        helemnt.InvokeMember("Click");
+                    }
+                }
+                beklet(5);
+                col = wb.Document.GetElementsByTagName("a");
+                foreach (HtmlElement helemnt in col)
+                {
+                    if (helemnt.GetAttribute("id") == "download")
+                    {
+                        try
+                        {
+                            using (WebClient client2 = new WebClient())
+                            {
+                                string filename = yazı.Text;
+                                try
+                                {
+                                    filename = filename.Substring(0, 206);
+                                }
+                                catch
+                                {
+
+                                }
+                                try
+                                {
+                                    filename = CleanFileName(filename);
+                                }
+                                catch
+                                {
+
+                                }
+                                client2.DownloadFile(helemnt.GetAttribute("href"), "images\\" + filename + ".jpg");
+                            }
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                goto editor;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try { Directory.CreateDirectory(Application.StartupPath + "\\images\\"); } catch { }
+        }
+
+        private void wait_Tick(object sender, EventArgs e)
+        {
+            wait.Enabled = false;
+        }
+    }
+}
